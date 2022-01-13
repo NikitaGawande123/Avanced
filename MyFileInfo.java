2d01cec8f5243fc15fd4115de1776cb1c2c48c75
NikitaGawande123 <96858213+NikitaGawande123@users.noreply.github.com>
2022-01-13 17:29:05 +0530

    Add files via upload

---
+++ MyFileInfo.java
@@ -0,0 +1,20 @@
+package AdvancedLevelFileHandling;
+
+import java.io.File;
+
+public class MyFileInfo {
+	
+	public static void main(String[] args) {
+	    File myObj = new File("Nickk.txt");
+	    if (myObj.exists()) {
+	      System.out.println("File name: " + myObj.getName());
+	      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+	      System.out.println("Writeable: " + myObj.canWrite());
+	      System.out.println("Readable " + myObj.canRead());
+	      System.out.println("File size in bytes " + myObj.length());
+	    } else {
+	      System.out.println("The file does not exist.");
+	    }
+	  }
+
+}
