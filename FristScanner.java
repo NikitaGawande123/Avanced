25eb20f466013a9871fba699944ebddb567ea9c6
NikitaGawande123 <96858213+NikitaGawande123@users.noreply.github.com>
2022-01-20 18:26:01 +0530

    Add files via upload

---
+++ FristScanner.java
@@ -0,0 +1,18 @@
+package AdvancedLevelScanner;
+
+import java.util.Scanner;
+
+public class FristScanner {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner myObj = new Scanner(System.in);  
+	    System.out.println("Enter username");
+
+	    String userName = myObj.nextLine(); 
+	    System.out.println("Username is: " + userName);
+
+	}
+
+}
