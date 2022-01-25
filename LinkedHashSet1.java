1905d094a38c69310046608fd53cf992dd696671
NikitaGawande123 <96858213+NikitaGawande123@users.noreply.github.com>
2022-01-25 18:35:07 +0530

    Add files via upload

---
+++ LinkedHashSet1.java
@@ -0,0 +1,22 @@
+package AdvancedLevelLinkedHashSet;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashSet1 {
+
+	public static void main(String[] args) {
+		
+		
+Set<String> food = new LinkedHashSet<>();
+		
+		food.add("Pizza");
+		food.add("Sandwich");
+		food.add("Burger");
+		food.add("Momos");
+		food.add("Rolls");
+		System.out.println("The original menu of foods: " +food);
+
+	}
+
+}
