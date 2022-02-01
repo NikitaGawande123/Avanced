003afa37f0a8eafd15dc5fdbdfc16ce6875b4933
NikitaGawande123 <96858213+NikitaGawande123@users.noreply.github.com>
2022-02-01 12:36:14 +0530

    Add files via upload

--- Consumer1.java
+++ Consumer1.java
@@ -2,22 +2,23 @@ package AdvancedLevelFunctionalProgrammingBInterface;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 
 public class Consumer1 {
 
 	public static void main(String[] args) {
-		Consumer<Integer> Multi = num -> System.out.println(num*8);
-		Multi.accept(10);
-
-		Consumer<List<Integer>> MultiNum = list -> {
+		BiConsumer<String, Integer> MultiAtribute = (text, num) -> System.out.println(text+num);
+		MultiAtribute.accept("Stock: ", 10);
+		MultiAtribute.accept("Price: $", 1800);
+		
+		BiConsumer<List<Integer>, Integer> MultiNum = (list, num) -> {
 			for(int i= 0; i< list.size(); i++) {
-				list.set(i, list.get(i) * 5);
+				list.set(i, list.get(i) * num);
 			}
 		};
 		
 		List<Integer> num = Arrays.asList(5, 4, 3, 2, 1);
-		MultiNum.accept(num);
+		MultiNum.accept(num, 6);
 		System.out.println(num);
 		
 	}
